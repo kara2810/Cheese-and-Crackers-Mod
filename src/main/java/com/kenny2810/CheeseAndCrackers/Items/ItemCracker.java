3a9331a67702936cb8f36226e99b7aecb8d718ef
kenny2810 <karl.x@hotmail.co.uk>
2014-06-21 14:35:34 +0300

    Added new Easter Egg crackers

--- src/main/java/com/kenny2810/CheeseAndCrackers/Items/ItemCracker.java
+++ src/main/java/com/kenny2810/CheeseAndCrackers/Items/ItemCracker.java
@@ -33,35 +33,5 @@ public class ItemCracker extends Item
     /**
      * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
      */
-    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
-    {
-        if (!par3EntityPlayer.capabilities.isCreativeMode)
-        {
-            --par1ItemStack.stackSize;
-        }
-
-        par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
-
-        if (!par2World.isRemote)
-        {
-            par2World.spawnEntityInWorld(new EntityCracker(par2World, par3EntityPlayer));
-        }
-
-        return par1ItemStack;
-    }
-    public void onUpdate(ItemStack itemstack, World world, Entity entity, int i, boolean flag)
-    {
-    	EntityPlayer player = (EntityPlayer) entity;
-    	ItemStack crackerStack = new ItemStack(CheeseAndCrackersMain.Cracker);
-    	
-    	if(player.getCurrentEquippedItem() != null && (player.getCurrentEquippedItem().isItemEqual(crackerStack))){
-    player.addPotionEffect((new PotionEffect(Potion.damageBoost.getId(), 3, 4)));
-    player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 3, 4)));
-    player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 3, 4)));
-    }
-    else
-    {
-    player.curePotionEffects(itemstack);
-    }
-    }
+   
 }
